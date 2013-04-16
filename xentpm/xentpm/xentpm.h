559018bce83bfa25e87fd69e956430884a2cb97d
Nehal Bandi <Nehal.bandi@citrix.com>
2013-04-16 14:16:00 -0700

    remove the password string

--- xentpm/xentpm/xentpm.h
+++ xentpm/xentpm/xentpm.h
@@ -11,8 +11,9 @@
 #include <openssl/pem.h>
 #include <trousers/trousers.h>
 
-#define OWNER_SECRET    "xenroot"
-
+#define KEY_FILE "/opt/xensource/tpm/tpm_key"
+#define KEY_SIZE 20
+#define KEY_HEX_SIZE 40
 int generate_aik(char *aik_blob_path); 
 int get_aik_pem(char *aik_blob_path);
 int get_aik_tcpa(char *aik_blob_path); 
@@ -23,4 +24,5 @@ int tpm_challenge(char *aik_blob_file, char *challenge);
 int get_ek();
 int get_ekcert();
 int print_base64(void* data, uint32_t len);
+int read_tpm_key(unsigned char *key, int keyLen);
 #endif
