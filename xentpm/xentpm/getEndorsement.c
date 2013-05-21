bae86d0b4f2b1ee1b4cfded7f1c6387cc4033a60
Nehal Bandi <Nehal.bandi@citrix.com>
2013-05-21 13:38:06 -0700

    adding the lisences to the files

--- xentpm/xentpm/getEndorsement.c
+++ xentpm/xentpm/getEndorsement.c
@@ -1,4 +1,32 @@
-/* vim: set ts=4 sw=4 et tw=78 fo=cqt wm=0: */
+/*
+ * Copyright (C) 2006-2009 Citrix Systems Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation; version 2.1 only. with the special
+ * exception on linking described in file LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ */
+
+/*
+ * Copyright (c) 2009 Hal Finney
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ */
+
 #include "xentpm.h"
 #define BSIZE   128
 
